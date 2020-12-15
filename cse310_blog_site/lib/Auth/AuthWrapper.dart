b0e441e7d19c2acddf000f7fca9c25a1fb5bc7be
Imran Sefat <imransefat@gmail.com>
2020-12-15 15:31:21 +0600

    verify page navigated

--- cse310_blog_site/lib/Auth/AuthWrapper.dart
+++ cse310_blog_site/lib/Auth/AuthWrapper.dart
@@ -1,6 +1,6 @@
 import 'package:cse310_blog_site/Screens/SignIn.dart';
+import 'package:cse310_blog_site/Screens/VerifyEmailScreen.dart';
 import 'package:cse310_blog_site/Timeline.dart';
-import 'package:cse310_blog_site/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,9 +11,13 @@ class AuthenticationWrapper extends StatelessWidget {
     final firebaseUser = context.watch<User>();
 
     if (firebaseUser != null) {
-      return Timeline(
-        firebaseUser: firebaseUser,
-      );
+      if (firebaseUser.emailVerified) {
+        return Timeline(
+          firebaseUser: firebaseUser,
+        );
+      } else {
+        return VerifyEmail();
+      }
     } else {
       return SignIn();
     }
